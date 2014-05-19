f4f6c18d999fcb3c0b0cca65c76a4885444131cc
kujenga <ataylor0123@gmail.com>
2014-05-19 09:40:16 -0400

    Added temporary webView for Facebook search

---
+++ WSO/WSOTempSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  WSOTempSearchViewController.h
+//  WSO
+//
+//  Created by Aaron Taylor on 5/19/14.
+//  Copyright (c) 2014 Williams Students Online. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WSOTempSearchViewController : UIViewController
+
+@end
---
+++ WSO/WSOTempSearchViewController.m
@@ -0,0 +1,61 @@
+//
+//  WSOTempSearchViewController.m
+//  WSO
+//
+//  Created by Aaron Taylor on 5/19/14.
+//  Copyright (c) 2014 Williams Students Online. All rights reserved.
+//
+
+#import "WSOTempSearchViewController.h"
+
+@interface WSOTempSearchViewController ()
+
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@end
+
+@implementation WSOTempSearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    _webView.delegate = self;
+}
+
+- (void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    NSString *fullURL = @"http://wso.williams.edu/facebook/";
+    NSURL *url = [NSURL URLWithString:fullURL];
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+    [_webView loadRequest:requestObj];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
